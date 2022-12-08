98af1b8d6601d0c0637dc355b6ad85b09e9ec15a
Malkoff <malkoff.official@gmail.com>
2022-12-09 00:26:39 +0300

    add

--- variables.py
+++ variables.py
@@ -1,1 +1,35 @@
+#Описание обучающегося курса Python
 greeting = 'Привет!'
+print(greeting)
+
+#str - строка
+name = 'Григорий'
+print(name)
+print(type(name))
+
+city = 'Майкоп'
+print(city)
+print(type(city))
+
+#int - целое число
+age = 30
+print(age)
+print(type(age))
+
+well = 'Количество пройденных курсов'
+print(well)
+amount_well = 1
+print(amount_well)
+print(type(amount_well))
+
+percent = 'Ваш процент изучения курса Python'
+print(percent)
+#float - число с плавающей точкой
+avg_percent = 2.5
+print(avg_percent)
+print(type(avg_percent))
+
+print('Обучающийся')
+#bool - логический тип (истина или ложь)
+is_student = True
+print(type(is_student))
