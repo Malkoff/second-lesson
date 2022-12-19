911782d7c51510ba0ac262a7711714d4c350aff1
Malkoff <malkoff.official@gmail.com>
2022-12-19 23:24:28 +0300

    add

--- borndayforever.py
+++ borndayforever.py
@@ -1,15 +1,12 @@
-#цикл год и день рождения А.С. Пушкина
+# цикл год и день рождения А.С. Пушкина
 
-answer_one = int (input('Какой год рождения А.С. Пушкина? '))
+answer_one = int(input('Какой год рождения А.С. Пушкина? '))
 
 while answer_one != 1799:
-    answer_one = int (input('Какой год рождения А.С Пушкина? '))
-
-    if  answer_one == 1799:
-        answer_two = int (input('Какой день рождения А.С. Пушкина? '))
-
-        while answer_two != 6:
-            answer_two = int (input('Какой день рождения А.С. Пушкина? '))
-
-            if answer_two == 6:
-                print('Верно')
+    answer_one = int(input('Какой год рождения А.С Пушкина? '))
+if answer_one == 1799:
+    answer_two = int(input('Какой день рождения А.С. Пушкина? '))
+    while answer_two != 6:
+        answer_two = int(input('Какой день рождения А.С. Пушкина? '))
+    if answer_two == 6:
+        print('Верно')
--- victory.py
+++ victory.py
@@ -1,88 +1,88 @@
-# Викторина
-greeting = 'Добро пожаловать на викторину! "Великие писатели'
-print(greeting)
-condition = 'Назовите года писателей!'
-print(condition)
-
-print('')
-# Правильный ответ: год рождения А.И. Куприн - 1870
-print('Вопрос 1: Какого года рождения А.И. Куприн?')
-answer1 = int(input('Ответ: '))
-# Правильный ответ: год рождения Н.В. Гоголь - 1809
-print('Вопрос 2: Какого года рождения Н.В. Гоголь?')
-answer2 = int(input('Ответ: '))
-# Правильный ответ: год рождения А.П. Чехов - 1860
-print('Вопрос 3: Какого года рождения А.П. Чехов?')
-answer3 = int(input('Ответ: '))
-# Правильный ответ: год рождения А.С. Пушкин - 1799
-print('Вопрос 4: Какого года рождения А.С. Пушкин?')
-answer4 = int(input('Ответ?: '))
-# Правильный ответ: год рождения М.Ю. Лермонтов - 1814
-print('Вопрос 5: Какого года рождения М.Ю. Лермонтов?')
-answer5 = int(input('Ответ: '))
-# Правильный ответ: год рождения Н.А. Некрасов - 1821
-print('Вопрос 6: Какого года рождения Н.А. Некрасов?')
-answer6 = int(input('Ответ: '))
-# Правильный ответ: год рождения И.С. Тургенев - 1818
-print('Вопрос 7: Какого года рождения И.С. Тургенев?')
-answer7 = int(input('Ответ: '))
-# Правильный ответ: год рождения Л.Н. Толстой - 1828
-print('Вопрос 8: Какого года рождения Л.Н. Толстой?')
-answer8 = int(input('Ответ: '))
-# Правильный ответ: год рождения И.А. Гончаров - 1812
-print('Вопрос 9: Какого года рождения И.А. Гончаров?')
-answer9 = int(input('Ответ: '))
-
-print('')
-# Расчеты
 right_answer = 0
 wrong_answer = 0
-if answer1 == 1870:
-    right_answer += 1
-else:
-    wrong_answer += 1
-if answer2 == 1809:
-    right_answer += 1
-else:
-    wrong_answer += 1
-if answer3 == 1860:
-    right_answer += 1
-else:
-    wrong_answer += 1
-if answer4 == 1799:
-    right_answer += 1
-else:
-    wrong_answer += 1
-if answer5 == 1814:
-    right_answer += 1
-else:
-    wrong_answer += 1
-if answer6 == 1821:
-    right_answer += 1
-else:
-    wrong_answer += 1
-if answer7 == 1818:
-    right_answer += 1
-else:
-    wrong_answer += 1
-if answer8 == 1828:
-    right_answer += 1
-else:
-    wrong_answer += 1
-if answer9 == 1812:
-    right_answer += 1
-else:
-    wrong_answer += 1
+greeting = 'Добро пожаловать на викторину'
+condition = 'Назовите года писателей!'
+
+start = input('Готовы?(да/нет): ')
+while (start == 'да'):
+    if start != 'да':
+        break
+    print('')
+    print(greeting)
+    print(condition)
+    print('')
+    print('Вопрос 1: Какого года рождения А.И. Куприн?')  # Правильный ответ: 1870
+    answer1 = int(input('Ответ: '))
+    if answer1 == 1870:
+        right_answer += 1
+    else:
+        wrong_answer += 1
+    print('Вопрос 2: Какого года рождения Н.В. Гоголь?')  # Правильный ответ: 1809
+    answer2 = int(input('Ответ: '))
+    if answer2 == 1809:
+        right_answer += 1
+    else:
+        wrong_answer += 1
+    print('Вопрос 3: Какого года рождения А.П. Чехов?')  # Правильный ответ: 1860
+    answer3 = int(input('Ответ: '))
+    if answer3 == 1860:
+        right_answer += 1
+    else:
+        wrong_answer += 1
+    print('Вопрос 4: Какого года рождения А.С. Пушкин?')  # Правильный ответ: 1799
+    answer4 = int(input('Ответ?: '))
+    if answer4 == 1799:
+        right_answer += 1
+    else:
+        wrong_answer += 1
+    print('Вопрос 5: Какого года рождения М.Ю. Лермонтов?')  # Правильный ответ: 1814
+    answer5 = int(input('Ответ: '))
+    if answer5 == 1814:
+        right_answer += 1
+    else:
+        wrong_answer += 1
+    print('Вопрос 6: Какого года рождения Н.А. Некрасов?')  # Правильный ответ: 1821
+    answer6 = int(input('Ответ: '))
+    if answer6 == 1821:
+        right_answer += 1
+    else:
+        wrong_answer += 1
+    print('Вопрос 7: Какого года рождения И.С. Тургенев?')  # Правильный ответ: 1818
+    answer7 = int(input('Ответ: '))
+    if answer7 == 1818:
+        right_answer += 1
+    else:
+        wrong_answer += 1
+    print('Вопрос 8: Какого года рождения Л.Н. Толстой?')  # Правильный ответ: 1828
+    answer8 = int(input('Ответ: '))
+    if answer8 == 1828:
+        right_answer += 1
+    else:
+        wrong_answer += 1
+    print('Вопрос 9: Какого года рождения И.А. Гончаров?')  # Правильный ответ: 1812
+    answer9 = int(input('Ответ: '))
+    if answer9 == 1812:
+        right_answer += 1
+    else:
+        wrong_answer += 1
+    print('Вопрос 10: Какого года рождения А.Н. Толстой?')  # Правильный ответ: 1883
+    answer10 = int(input('Ответ: '))
+    if answer10 == 1883:
+        right_answer += 1
+    else:
+        wrong_answer += 1
+    print('')
+    print('Результаты')
+    print('Правильных ответов', right_answer)
+    print('Неправильных ответов', wrong_answer)
+    print('Процент правильных ответов', right_answer * 100 / 10)
+    print('Процент неправильных ответов', wrong_answer * 100 / 10)
+
+    print('')
+    restart = input('Сыграть ещё раз?(да/нет): ')
+    if restart != 'да':
+        break
+    start = input('Готовы?(да/нет): ')
 
 print('')
-# Результаты
-print('Результаты')
-print('')
-print('Правильных ответов', right_answer)
-print('Неправильных ответов', wrong_answer)
-print('Процент правильных ответов', right_answer * 100 / 9)
-print('Процент неправильных ответов', wrong_answer * 100 / 9)
-
-print('')
-# Сыграть ещё раз
-questions = input('Желаете сыграть ещё раз?(да/нет): ')
+print('Спасибо за участие!')
